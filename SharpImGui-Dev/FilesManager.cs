d569d630e79b9a0b2c0ce68300c745e02a8acc4e
MasterSix <zanohelp132@gmail.com>
2024-11-23 11:02:47 -0300

    Update StoragePair

--- SharpImGui-Dev/FilesManager.cs
+++ SharpImGui-Dev/FilesManager.cs
@@ -3,29 +3,29 @@
 public static class FilesManager
 {
     private static readonly string ProjectPath = Path.Combine("../../../");
-    private static readonly string _nativesDirectory = Path.Combine(ProjectPath, "dcimgui");
-    private static readonly string _outputDirectory = Path.Combine(ProjectPath, "../Plugins/dcimgui");
+    private static readonly string NativesDirectory = Path.Combine(ProjectPath, "dcimgui");
+    private static readonly string OutputDirectory = Path.Combine(ProjectPath, "../SharpImGui/Plugins/dcimgui");
     
     public static void CopyNativesToOutputDirectory()
     {
-        if (Directory.Exists(_outputDirectory))
-            Directory.Delete(_outputDirectory, true);
+        if (Directory.Exists(OutputDirectory))
+            Directory.Delete(OutputDirectory, true);
         
-        Directory.CreateDirectory(_outputDirectory);
+        Directory.CreateDirectory(OutputDirectory);
         //dcimgui_x64.dll
         //dcimgui_x86.dll
         //dcimgui.so
         //dcimgui.dylib
         //libdcimgui_arm.dll
         //libdcimgui_x86.dll
-        var files = Directory.GetFiles(_nativesDirectory);
+        var files = Directory.GetFiles(NativesDirectory);
         foreach (var file in files)
         {
             var fileName = Path.GetFileName(file);
             if (fileName.EndsWith(".json") || fileName.EndsWith(".h") || fileName.EndsWith(".cpp"))
                 continue;
             
-            var platformPath = Path.Combine(_outputDirectory, fileName switch
+            var platformPath = Path.Combine(OutputDirectory, fileName switch
             {
                 "dcimgui_x64.dll" => "win-x64",
                 "dcimgui_x86.dll" => "win-x86",
